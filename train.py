ccf8cb79d6a240378b359f009335c9965e7e7ecc
aravinthakshan <aaravinth2005@gmail.com>
2025-09-25 19:52:04 +0530

    minor change cbsd68 evaluate function to be changed

--- train.py
+++ train.py
@@ -19,6 +19,8 @@ def main():
 
     args = parser.parse_args()
 
+    # make sure you are cropping here as well centre prop
+    
     noisy_img = Image.open(args.noisy_img).convert("RGB")
     clean_img = Image.open(args.clean_img).convert("RGB")
 
