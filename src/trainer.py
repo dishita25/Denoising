4babc8810a67b5da74d1c1394ae6a03399f3b065
aravinthakshan <aaravinth2005@gmail.com>
2025-09-25 19:03:34 +0530

    added necessary things

--- src/trainer.py
+++ src/trainer.py
@@ -2,7 +2,22 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 from src.loss import loss_func
+from model.ZSN2N import network
 from src.loss import mse
+import torch.optim as optim
+
+max_epoch = 5000     # training epochs
+lr = 0.001           # learning rate
+step_size = 1000     # number of epochs at which learning rate decays
+gamma = 0.5          # factor by which learning rate decays
+device = 'cuda'
+
+n_chan = clean_img.shape[1]
+model = network(n_chan)
+model = model.to(device)
+print("The number of parameters of the network is: ",  sum(p.numel() for p in model.parameters() if p.requires_grad))
+optimizer = optim.Adam(model.parameters(), lr=lr)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
 
 def train(model, optimizer, noisy_img):
 
