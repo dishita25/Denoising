e3df04723cb247606fdff64cd81f7e6fd1c222e4
aravinthakshan <aaravinth2005@gmail.com>
2025-09-25 19:48:35 +0530

    changed chan embed

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ def main():
     parser.add_argument("--step_size", type=int, default=1000, help="LR step size")
     parser.add_argument("--gamma", type=float, default=0.5, help="LR decay factor")
     parser.add_argument("--device", type=str, default="cuda", help="Device to use")
-    parser.add_argument("--n_chan", type=int, default=None, help="Number of channels (auto if None)")
+    parser.add_argument("--n_chan", type=int, default=48, help="Number of channels (auto if None)")
     parser.add_argument("--noisy_img", type=str, default=None, help="Noisy Image")
     parser.add_argument("--clean_img", type=str, default=None, help="Clean Image")
     parser.add_argument("--dataset", type=str, default=None, help="Dataset Name")
@@ -37,6 +37,6 @@ def main():
     results = test_model(model, args.dataset)
 
     print("Test Results:", results)
-    
+
 if __name__ == "__main__":
     main()
